c6b2ebde3b42232c7e9149ded8d6faf460775870
atoenggDev <kromosom06@gmail.com>
2024-08-29 23:29:28 +0700

    added: element button

---
+++ src/components/elements/Buttons.tsx
@@ -0,0 +1,13 @@
+import { ButtonProps } from "@/types"
+import { Button } from "flowbite-react"
+
+
+export const Buttons = ({className, color, size, label}: ButtonProps) => {
+    return (
+        <>
+            <Button color={color} size={size} pill className={className}>
+                {label}
+            </Button>
+        </>
+    )
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,13 @@
 import { StaticImageData } from "next/image";
 
 export interface euroLogo {
-    src: string | StaticImageData;
-    className?: string;
-  }
+  src: string | StaticImageData;
+  className?: string;
+}
+
+export interface ButtonProps {
+  className?: string;
+  color?: "blue" | "gray" | "dark" | "light" | "success" | "failure" | "warning" | "purple";
+  label?: string;
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+}
