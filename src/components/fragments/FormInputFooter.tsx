861db98b9d84ebfe7393772c6ca36c3b697991b3
atoenggDev <kromosom06@gmail.com>
2024-09-03 14:57:56 +0700

    update: font size in input footer

--- src/components/fragments/FormInputFooter.tsx
+++ src/components/fragments/FormInputFooter.tsx
@@ -12,7 +12,7 @@ export const FormInputFooter = () => {
                     name="bacodan"
                     id="bacodan"
                     placeholder="masukan bacod-an anda"
-                    className="w-full bg-white text-secondary-950 rounded-full border border-solid border-black text-xs py-2 px-4 mr-2"
+                    className="w-full bg-white text-secondary-950 rounded-full border border-solid border-black text-[9px] py-2 px-4 mr-2"
                 />
                 <Buttons size="xs" className="bg-primary-500 p-1 rounded-full flex items-center justify-center text-white duration-200 transition-all ease-in-out hover:bg-primary-600" label={<BsSendFill className="h-4 w-4" />} />
             </form>
