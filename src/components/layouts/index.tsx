3a10abe11814e3d48ae064e7ffaa6ad783ed2b66
atoenggDev <kromosom06@gmail.com>
2024-12-21 21:23:46 +0700

    add: music

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,14 +1,22 @@
+"use client"
+
 import { AuthProvider } from "@/context/AuthContext";
 import { Navbar } from "../fragments/Navbar";
+import { ButtonPlayMusic } from "../elements/button/ButtonPlayMusic";
 
 export const Layouts = ({ children }: { children: React.ReactNode }) => {
+
+  
   return (
     <>
-    <AuthProvider>
-      <Navbar />
-      <main className="bg-secondary-950">
-        {children}
-      </main>
+      <AuthProvider>
+        <Navbar />
+        <main className="bg-secondary-950">
+          {children}
+        </main>
+        
+        <ButtonPlayMusic/>
+
       </AuthProvider>
     </>
   );
